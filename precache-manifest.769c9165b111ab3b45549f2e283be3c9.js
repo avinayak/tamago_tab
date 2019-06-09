0d783245a97f08c33b3ff08446e28d3d7688ce56
Atul <atul@tablecheck.com>
2019-06-09 20:41:43 +0900

    Updates

---
+++ precache-manifest.769c9165b111ab3b45549f2e283be3c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d773476f44203fc213df071d0752ca11",
+    "url": "/tamago_tab/index.html"
+  },
+  {
+    "revision": "8728b08be9b1d537aed4",
+    "url": "/tamago_tab/static/css/main.04770fce.chunk.css"
+  },
+  {
+    "revision": "1cee507bf500fb84f7fa",
+    "url": "/tamago_tab/static/js/2.3336c636.chunk.js"
+  },
+  {
+    "revision": "8728b08be9b1d537aed4",
+    "url": "/tamago_tab/static/js/main.9b524ab7.chunk.js"
+  },
+  {
+    "revision": "25916276aa1ced055510",
+    "url": "/tamago_tab/static/js/runtime~main.9884882e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tamago_tab/static/media/logo.5d5d9eef.svg"
+  }
+]);
